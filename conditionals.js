1a3cb8c3af34bffe89b7e4e9c0809bb719b93a59
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-08-22 04:13:43 +0000

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -30,3 +30,20 @@ else if (temp < 15){
 else{
   console.log("Shorts are fine");
 }
+
+const isCitizen = true;
+const age = 21;
+
+if(isCitizen && age > 18){
+  console.log("You can vote")
+}
+
+const temperature = -60
+if (temperature < 40 || temperature > 40){
+  console.log("Don't go out")
+}
+
+raining = false;
+if(!raining){
+  console.log("Get wet!")
+}
